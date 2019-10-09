03ee79af4c871ac78eecf0360e8755d3966ff546
JulOleks <bilous.yuliia@gmail.com>
2019-10-09 15:57:01 +0300

    change component output in Dialog into map loop

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,39 +1,43 @@
 import React from "react";
-import {NavLink} from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 
-const DialogItem = (props)=>{
-	return(
+const DialogItem = (props) => {
+	return (
 		<div className="item">
 			<NavLink to={`/dialogs/${props.id}`}>{props.name}</NavLink>
 		</div>
 	)
-} 
-
-const Message =(props)=>{
-	return(
-	<div className="message">
-				{props.message}
-	</div>
-	 )
 }
 
-const Dialogs = (props)=>{
-	return(
-	<div className="dialogs">
-		<div className="dialogs__items">
-			<DialogItem name="Den" id="1" /> 
-			<DialogItem name="Karen" id="2"/> 
-			<DialogItem name="Sun" id="3"/> 
-			<DialogItem name="Sam" id="4"/> 
+const Message = (props) => {
+	return (
+		<div className="message">
+			{props.message}
+		</div>
+	)
+}
 
+const Dialogs = (props) => {
+	let dataDialog =
+		[{ id: 1, name: "Den" }, { id: 2, name: "Karen" }, { id: 3, name: "Sun" }, { id: 4, name: "Sam" }];
+
+	let dialogelem = dataDialog.map(d => (<DialogItem name={d.name} id={d.id} />))
+
+	let dataMessage =
+		[{ id: 1, message: "hi" }, { id: 2, message: "Its goo idea" }, { id: 3, message: "Nice react" }];
+	let messageselem = dataMessage.map(m => (<Message message={m.message} />))
+
+	return (
+		<div className="dialogs">
+			<div className="dialogs__items">
+				{dialogelem}
+
+			</div>
+			<div className="dialogs__messages">
+				{messageselem}
+			</div>
 		</div>
-		<div className="dialogs__messages">
-			<Message message="hi" />
-			<Message message="Its goo idea" />
-			<Message message="Nice react" />
-		</div>
-	</div>
 
 	)
 }
--- src/components/Profile/My Posts/MyPosts.jsx
+++ src/components/Profile/My Posts/MyPosts.jsx
@@ -2,25 +2,25 @@ import React from "react";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
-
-  return (
-    <div className="postsblock">
-		<h3>My Posts</h3>
-		<div>
-			<div className="postsBlock__textarea"><textarea></textarea>
+let dataPost=[{id:1, message:"Hello, how are you?", likesCount:17}, {id:1, message:"Today is good weather",likesCount:23}]
+	return (
+		<div className="postsblock">
+			<h3>My Posts</h3>
+			<div>
+				<div className="postsBlock__textarea"><textarea></textarea>
+				</div> 
+				<div className="postsblock__buttons">
+					<button>Add post</button>
+					<button>Delete</button>
+				</div>
+				<div className="postsblock__posts">
+					<Post message= {dataPost[0].message} like={dataPost[0].likesCount} />
+					<Post message= {dataPost[1].message} like={dataPost[1].likesCount} />
+				</div>
 			</div>
-		<div className="postsblock__buttons">
-			<button>Add post</button>
-			<button>Delete</button>
 		</div>
-		<div className="postsblock__posts">
-			<Post message="Hello, how are you?" like="17"/>	
-			<Post message="Today is good weather" like="23"/>
-		</div>
-	</div>
-</div>
-     
-  );
+
+	);
 };
 
 export default MyPosts;
